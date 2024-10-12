0c10bc4c32b277eae379481d2e1ac0c95b705e12
Mark Zhebelev <mrkzheb@gmail.com>
2024-10-12 16:32:49 +0300

    update getData.test.ts

--- src/modules/AnimationBlock/api/getData.test.ts
+++ src/modules/AnimationBlock/api/getData.test.ts
@@ -1,7 +1,29 @@
 import {getData} from './getData';
+import axios from 'axios';
+jest.mock('axios');
 
 test('getData should return an array of objects with expected structure', async () => {
+
+    const mockedData = [
+        {
+            id: "1",
+            start: 1980,
+            end: 1985,
+            nameEvents: "Тех. события 1980-1985",
+            events: [
+                {
+                    id: "1",
+                    year: 1982,
+                    text: "Выпущен Commodore 64 — самый продаваемый компьютер в истории.",
+                },
+            ],
+        },
+    ];
+
+    (axios.get as jest.Mock).mockResolvedValue({ status: 200, data: mockedData });
+
     const data = await getData();
+
     expect(data).toEqual(expect.arrayContaining(
         [expect.objectContaining(
             {
